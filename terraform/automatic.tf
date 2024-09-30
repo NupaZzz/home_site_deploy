384c23cf0a6ee2397dd97ce231122d954121b7f3
NupaZzz <nupalolz@gmail.com>
2024-10-01 01:54:10 +0300

    "Extracted SSH connection details into a separate provider block and updated resource 'deploy_container' to use it."

--- terraform/automatic.tf
+++ terraform/automatic.tf
@@ -1,12 +1,13 @@
-resource "ssh_resource" "deploy_container" {
-  provisioner "remote-exec" {
-    host        = "192.168.100.7"
-    user        = "Arinka"
-    private_key = file("/home/asus/.ssh/id_rsa")
-    timeout     = "1m"
+provider "ssh" {
+  alias   = "deploy"
+  host    = "192.168.100.7"
+  user    = "Arinka"
+  private_key = file("/home/asus/.ssh/id_rsa")
+  timeout = "1m"
+}
 
-    commands = [
-      "docker run -d -p 8080:80 --name nginx nginx:latest"
-    ]
-  }
+resource "ssh_resource" "deploy_container" {
+  provider = ssh.deploy
+
+  command = "docker run -d -p 8080:80 --name nginx nginx:latest"
 }
