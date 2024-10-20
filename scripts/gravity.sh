f583fa0506e0ff33144a1252265a807ad4807d55
NupaZzz <Nupalolz@gmail.com>
2024-10-20 15:53:29 +0300

    "Update pihole_setup Ansible role and gravity.sh script: modify task conditions, add executable checks, and fix script syntax"

--- scripts/gravity.sh
+++ scripts/gravity.sh
@@ -2,21 +2,19 @@
 
 PIHOLE_CONTAINER="pihole"
 
-if ! command -v docker &> /dev/null
-then
+if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Please install Docker and try again."
     exit 1
 fi
 
-if ! docker ps -a --format '{{.Names}}' | grep -Eq "^${PIHOLE_CONTAINER}$"
-then
+if ! docker ps -a --format '{{.Names}}' | grep -Eq "^${PIHOLE_CONTAINER}$"; then
     echo "Container '${PIHOLE_CONTAINER}' does not exist. Please check the container name and try again."
     exit 1
 fi
 
-docker exec -it ${PIHOLE_CONTAINER} pihole -g
+docker exec -it "${PIHOLE_CONTAINER}" pihole -g
 
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
     echo "$(date): Pi-hole gravity list update completed successfully." | tee -a /var/log/pihole/pihole_update.log
 else
     echo "$(date): Pi-hole gravity list update failed." | tee -a /var/log/pihole/pihole_update.log
